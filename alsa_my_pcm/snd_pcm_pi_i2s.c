84d49d85132f446b40c6fbce2693c1276914b3ce
Jollen Chen <jollen@moko365.com>
2017-09-13 18:05:52 +0800

    add sample: ALSA PCM

---
+++ alsa_my_pcm/snd_pcm_pi_i2s.c
@@ -0,0 +1,319 @@
+#include <linux/module.h>
+#include <linux/version.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/fs.h>
+#include <linux/delay.h>
+#include <linux/vmalloc.h>
+#include <linux/sched.h>
+#include <linux/timer.h>
+#include <linux/mm.h>
+#include <linux/interrupt.h>
+#include <linux/irq.h>
+#include <linux/miscdevice.h>
+#include <linux/input.h>
+#include <linux/slab.h>
+#include <linux/platform_device.h>
+#include <linux/debugfs.h>
+#include <linux/spinlock.h>
+#include <linux/workqueue.h>
+#include <asm/io.h>
+#include <asm/uaccess.h>
+
+#include <sound/core.h>
+#include <sound/control.h>
+#include <sound/tlv.h>
+#include <sound/pcm.h>
+#include <sound/info.h>
+#include <sound/initval.h>
+
+MODULE_AUTHOR("Jollen Chen");
+MODULE_DESCRIPTION("PCM ALSA driver template");
+MODULE_LICENSE("GPL");
+
+static int index[SNDRV_CARDS] = SNDRV_DEFAULT_IDX;	/* Index 0-MAX */
+static char *id[SNDRV_CARDS] = SNDRV_DEFAULT_STR;	/* ID for this card */
+
+struct mychip {
+	struct snd_card *card;
+	struct snd_pcm *pcm;
+};
+
+/* hardware definition */
+static struct snd_pcm_hardware snd_mychip_playback_hw = {
+        .info = (SNDRV_PCM_INFO_MMAP |
+                 SNDRV_PCM_INFO_INTERLEAVED |
+                 SNDRV_PCM_INFO_BLOCK_TRANSFER |
+                 SNDRV_PCM_INFO_MMAP_VALID),
+        .formats =          SNDRV_PCM_FMTBIT_S16_LE,
+        .rates =            SNDRV_PCM_RATE_8000_48000,
+        .rate_min =         8000,
+        .rate_max =         48000,
+        .channels_min =     2,
+        .channels_max =     2,
+        .buffer_bytes_max = 32768,
+        .period_bytes_min = 4096,
+        .period_bytes_max = 32768,
+        .periods_min =      1,
+        .periods_max =      1024,
+};
+
+/* hardware definition */
+static struct snd_pcm_hardware snd_mychip_capture_hw = {
+        .info = (SNDRV_PCM_INFO_MMAP |
+                 SNDRV_PCM_INFO_INTERLEAVED |
+                 SNDRV_PCM_INFO_BLOCK_TRANSFER |
+                 SNDRV_PCM_INFO_MMAP_VALID),
+        .formats =          SNDRV_PCM_FMTBIT_S16_LE,
+        .rates =            SNDRV_PCM_RATE_8000_48000,
+        .rate_min =         8000,
+        .rate_max =         48000,
+        .channels_min =     2,
+        .channels_max =     2,
+        .buffer_bytes_max = 32768,
+        .period_bytes_min = 4096,
+        .period_bytes_max = 32768,
+        .periods_min =      1,
+        .periods_max =      1024,
+};
+
+/* open callback */
+static int snd_mychip_playback_open(struct snd_pcm_substream *substream)
+{
+        struct mychip *chip = snd_pcm_substream_chip(substream);
+        struct snd_pcm_runtime *runtime = substream->runtime;
+
+        runtime->hw = snd_mychip_playback_hw;
+        /* more hardware-initialization will be done here */
+	printk(KERN_INFO "snd_mychip_playback_open\n");
+        return 0;
+}
+
+/* close callback */
+static int snd_mychip_playback_close(struct snd_pcm_substream *substream)
+{
+        struct mychip *chip = snd_pcm_substream_chip(substream);
+        /* the hardware-specific codes will be here */
+        return 0;
+
+}
+
+/* open callback */
+static int snd_mychip_capture_open(struct snd_pcm_substream *substream)
+{
+        struct mychip *chip = snd_pcm_substream_chip(substream);
+        struct snd_pcm_runtime *runtime = substream->runtime;
+
+        runtime->hw = snd_mychip_capture_hw;
+        /* more hardware-initialization will be done here */
+        return 0;
+}
+
+/* close callback */
+static int snd_mychip_capture_close(struct snd_pcm_substream *substream)
+{
+        struct mychip *chip = snd_pcm_substream_chip(substream);
+        /* the hardware-specific codes will be here */
+        return 0;
+
+}
+
+/* hw_params callback */
+static int snd_mychip_pcm_hw_params(struct snd_pcm_substream *substream,
+                             struct snd_pcm_hw_params *hw_params)
+{
+     	printk(KERN_INFO "snd_mychip_pcm_hw_params: \n");
+
+        return snd_pcm_lib_malloc_pages(substream,
+                                   params_buffer_bytes(hw_params));
+}
+
+/* hw_free callback */
+static int snd_mychip_pcm_hw_free(struct snd_pcm_substream *substream)
+{
+        return snd_pcm_lib_free_pages(substream);
+}
+
+/* prepare callback */
+static int snd_mychip_pcm_prepare(struct snd_pcm_substream *substream)
+{
+        struct mychip *chip = snd_pcm_substream_chip(substream);
+        struct snd_pcm_runtime *runtime = substream->runtime;
+
+        /* set up the hardware with the current configuration
+         * for example...
+         */
+        return 0;
+}
+
+/* trigger callback */
+static int snd_mychip_pcm_trigger(struct snd_pcm_substream *substream,
+                                  int cmd)
+{
+        switch (cmd) {
+        case SNDRV_PCM_TRIGGER_START:
+                /* do something to start the PCM engine */
+                break;
+        case SNDRV_PCM_TRIGGER_STOP:
+                /* do something to stop the PCM engine */
+                break;
+        default:
+                return -EINVAL;
+        }
+}
+
+static unsigned int current_ptr = 0;
+
+/* pointer callback */
+static snd_pcm_uframes_t
+snd_mychip_pcm_pointer(struct snd_pcm_substream *substream)
+{
+        struct mychip *chip = snd_pcm_substream_chip(substream);
+        unsigned int current_ptr;
+
+        /* get the current hardware pointer */
+        return (snd_pcm_uframes_t) current_ptr;
+}
+
+/* operators */
+static struct snd_pcm_ops snd_mychip_playback_ops = {
+        .open =        snd_mychip_playback_open,
+        .close =       snd_mychip_playback_close,
+        .ioctl =       snd_pcm_lib_ioctl,
+        .hw_params =   snd_mychip_pcm_hw_params,
+        .hw_free =     snd_mychip_pcm_hw_free,
+        .prepare =     snd_mychip_pcm_prepare,
+        .trigger =     snd_mychip_pcm_trigger,
+        .pointer =     snd_mychip_pcm_pointer,
+};
+
+static int snd_mychip_pcm_new(struct mychip *mychip, int device,
+					int substreams)
+{
+	struct snd_pcm *pcm;
+	struct snd_pcm_ops *ops;
+	int err;
+
+	err = snd_pcm_new(mychip->card, "LinkIt 7688 PCM", device,
+			       substreams, substreams, &pcm);
+	if (err < 0)
+		return err;
+	mychip->pcm = pcm;
+	ops = &snd_mychip_playback_ops;
+	snd_pcm_set_ops(pcm, SNDRV_PCM_STREAM_PLAYBACK, ops);
+	pcm->private_data = mychip;
+	pcm->info_flags = 0;
+	strcpy(pcm->name, "LinkIt 7688 PCM I2S");
+
+	return 0;
+}
+
+/* operators */
+static struct snd_pcm_ops snd_mychip_capture_ops = {
+        .open =        snd_mychip_capture_open,
+        .close =       snd_mychip_capture_close,
+        .ioctl =       snd_pcm_lib_ioctl,
+        .hw_params =   snd_mychip_pcm_hw_params,
+        .hw_free =     snd_mychip_pcm_hw_free,
+        .prepare =     snd_mychip_pcm_prepare,
+        .trigger =     snd_mychip_pcm_trigger,
+        .pointer =     snd_mychip_pcm_pointer,
+};
+
+/*
+ *  definitions of capture are omitted here...
+ */
+
+/* create a pcm device */
+static int snd_mychip_new_pcm(struct mychip *chip)
+{
+        struct snd_pcm *pcm;
+        int err;
+
+        err = snd_pcm_new(chip->card, "My Chip", 0, 1, 1, &pcm);
+        if (err < 0)
+                return err;
+        pcm->private_data = chip;
+        strcpy(pcm->name, "My Chip");
+        chip->pcm = pcm;
+        /* set operators */
+        snd_pcm_set_ops(pcm, SNDRV_PCM_STREAM_PLAYBACK,
+                        &snd_mychip_playback_ops);
+        snd_pcm_set_ops(pcm, SNDRV_PCM_STREAM_CAPTURE,
+                        &snd_mychip_capture_ops);
+        /* pre-allocation of buffers */
+        return 0;
+}
+
+/******** Platform Driver ********/
+
+static int snd_pi_i2s_probe(struct platform_device *devptr)
+{
+	struct mychip *mychip;
+	struct snd_card *card;
+	int err;
+
+	err = snd_card_create(index[devptr->id], id[devptr->id], THIS_MODULE,
+			      sizeof(struct mychip), &card);
+	if (err < 0)
+		return err;
+
+	mychip = card->private_data;
+	mychip->card = card;
+
+        err = snd_mychip_pcm_new(mychip, 0 /* device number */, 1 /* number of streams */);
+        if (err < 0)
+	    goto __nodev;
+
+	strcpy(card->driver, "LinkIt 7688 PCM");
+	strcpy(card->shortname, "LinkIt 7688 I2S");
+	sprintf(card->longname, "LinkIt I2S PCM Driver %i", devptr->id);
+
+	snd_card_set_dev(card, &devptr->dev);
+
+	err = snd_card_register(card);
+	if (err == 0) {
+		platform_set_drvdata(devptr, card);
+		return 0;
+	}
+      __nodev:
+	snd_card_free(card);
+	return err;
+}
+
+static int snd_pi_i2s_remove(struct platform_device *devptr)
+{
+	snd_card_free(platform_get_drvdata(devptr));
+	platform_set_drvdata(devptr, NULL);
+	return 0;
+}
+
+#define SND_PI_I2S_DRIVER	"snd_pi_i2s_pcm"
+
+static struct platform_driver snd_pi_i2s_driver = {
+    .probe = snd_pi_i2s_probe,
+    .remove = snd_pi_i2s_remove,
+    .driver = {
+        .name = SND_PI_I2S_DRIVER
+    },
+};
+
+static int __init alsa_card_pi_i2s_init(void)
+{
+	int  cards, err = 1;
+
+	err = platform_driver_register(&snd_pi_i2s_driver);
+	if (err < 0)
+		return err;
+
+	printk(KERN_INFO "alsa_my_pcm registered.\n");
+	return 0;
+}
+
+static void __exit alsa_card_pi_i2s_exit(void)
+{
+	platform_driver_unregister(&snd_pi_i2s_driver);
+}
+
+module_init(alsa_card_pi_i2s_init)
+module_exit(alsa_card_pi_i2s_exit)
